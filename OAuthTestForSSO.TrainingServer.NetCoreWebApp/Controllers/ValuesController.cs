f0dc588bddac94f75cf5704362d384abc2e11f10
MonsterZam <github@1click.lv>
2022-02-20 19:05:52 +0200

    Initial commit

---
+++ OAuthTestForSSO.TrainingServer.NetCoreWebApp/Controllers/ValuesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace EncryptionServer.NetCoreWebApp.Controllers
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        ///     Self test
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/SelfTest")]
+        public ActionResult<string> Get()
+        {
+            return "VTE SYSTEM IS READY";
+        }
+
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/DoMagic")]
+        public ActionResult<string> DoMagic()
+        {
+            string result = String.Empty;
+            IQueryCollection queryCollection = HttpContext.Request.Query;
+
+            foreach (KeyValuePair<string, StringValues> keyValuePairs in queryCollection)
+            {
+                result += keyValuePairs.Key + "=" + keyValuePairs.Value + "\n";
+            }
+
+            return "SELF TEST:\n" + result;
+        }
+
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/DoMagic2")]
+        public ActionResult<string> DoMagic2()
+        {
+            string result = String.Empty;
+            IQueryCollection queryCollection = HttpContext.Request.Query;
+
+            foreach (KeyValuePair<string, StringValues> keyValuePairs in queryCollection)
+            {
+                result += keyValuePairs.Key + "=" + keyValuePairs.Value + "\n";
+            }
+
+            return "SELF TEST 2:\n" + result;
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/Handshake")]
+        public ActionResult<string> Handshake()
+        {
+            IFormCollection formCollection = HttpContext.Request.Form;
+
+            foreach (KeyValuePair<string, StringValues> keyValuePairs in formCollection)
+            {
+                string value = String.Empty;
+                if (keyValuePairs.Key == "Authorization")
+                {
+                    value = keyValuePairs.Value.ToString();
+                }
+                else
+                if (keyValuePairs.Key == "grant_type")
+                {
+                    value = keyValuePairs.Value.ToString();
+                }
+            }
+
+            return "100500!";
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/SecureRequest")]
+        public ActionResult<string> SecureRequest()
+        {
+            string result = String.Empty;
+            IQueryCollection queryCollection = HttpContext.Request.Query;
+
+            foreach (KeyValuePair<string, StringValues> keyValuePairs in queryCollection)
+            {
+                result += keyValuePairs.Key + "=" + keyValuePairs.Value + "\n";
+            }
+
+            return "SELF TEST 3:\n" + result;
+            //IFormCollection formCollection = HttpContext.Request.Form;
+
+            //foreach (KeyValuePair<string, StringValues> keyValuePairs in formCollection)
+            //{
+
+            //}
+
+            //return "100500!";
+        }
+    }
+}
---
+++ OAuthTestForSSO/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OAuthTestForSSO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Generate Authorize Access Token to authenticate REST Web API.  
+            string result = GetAuthorizeToken(txtAddress.Text).Result;
+
+            txtLog.Text += result;
+
+            // Call REST Web API method with authorize access token.  
+            string responseObj = GetInfo(txtAddress.Text, result).Result;
+
+            txtLog.Text += responseObj;
+        }
+
+        public static async Task<string> GetAuthorizeToken(string address)
+        {
+
+            // Initialization.  
+            string responseObj = string.Empty;
+
+            // Posting.  
+            using (HttpClient client = new HttpClient())
+            {
+                // Setting Base address.  
+                client.BaseAddress = new Uri(address);
+
+                // Setting content type.  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // Initialization.  
+                HttpResponseMessage response = new HttpResponseMessage();
+                List<KeyValuePair<string, string>> allIputParams = new List<KeyValuePair<string, string>>();
+
+                // Convert Request Params to Key Value Pair.  
+                allIputParams.Add(new KeyValuePair<string, string>("Authorization", "Basic " + "User name:User Secret" + ""));
+                allIputParams.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+
+                // URL Request parameters.  
+                HttpContent requestParams = new FormUrlEncodedContent(allIputParams);
+
+                // HTTP POST  
+                response = await client.PostAsync("api/Handshake", requestParams).ConfigureAwait(false);
+
+                // Verification  
+                if (response.IsSuccessStatusCode)
+                {
+                    // Reading Response.  
+                   responseObj = response.ToString();
+                }
+            }
+
+            return responseObj;
+        }
+
+        public static async Task<string> GetInfo(string address, string authorizeToken)
+        {
+            // Initialization.  
+            string responseObj = string.Empty;
+
+            // HTTP GET.  
+            using (HttpClient client = new HttpClient())
+            {
+                // Initialization
+                string authorization = authorizeToken;
+
+                // Setting Authorization
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authorization);
+
+                // Setting Base address
+                client.BaseAddress = new Uri(address);
+
+                // Setting content type
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // Initialization
+                HttpResponseMessage response = new HttpResponseMessage();
+
+                // HTTP GET  
+                response = await client.GetAsync("api/SecureRequest").ConfigureAwait(false);
+
+                // Verification  
+                if (response.IsSuccessStatusCode)
+                {
+                    // Reading Response.  
+                    responseObj = response.ToString();
+                }
+            }
+
+            return responseObj;
+        }
+    }
+}
